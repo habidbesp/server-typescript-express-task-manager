9247a956addb47081dee78e0a92f6f46ca58d47e
Habid B Espinosa <hab.bad@hotmail.com>
2024-12-16 21:26:30 +0100

    authentication with creating account and email confirmation

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
+import authRoutes from "./routes/authRoutes";
 import morgan from "morgan";
 
 dotenv.config();
@@ -21,6 +22,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 // Routes
+app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 
 export default app;
